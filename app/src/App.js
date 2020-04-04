10cf0d4a91a56471e0dc4bb437e802485b3417cc
Tarek Ali <tarekali15@outlook.com>
2020-04-04 12:51:28 -0500

    changed input and button size; edited stylesheets

--- app/src/App.js
+++ app/src/App.js
@@ -7,8 +7,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <form class="form-inline my-2 my-lg-0">
-          <input class="form-control mr-sm-2" type="text" placeholder="Search by name"></input>
-          <input class="btn btn-secondary" type="submit" value="search"></input>
+          <input class="form-control form-control-lg" type="text" placeholder="Search by name"></input>
+          <input class="btn btn-secondary btn-lg" type="submit" value="search"></input>
         </form>
       </header>
     </div>
