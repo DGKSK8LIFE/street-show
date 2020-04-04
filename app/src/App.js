1713da2b46e534278298f7503aa384fdf7faefe7
Tarek Ali <tarekali15@outlook.com>
2020-04-04 10:27:17 -0500

    stylized input

--- app/src/App.js
+++ app/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
+import './bootstrap.css'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <form>
-          <input class="search" type="text" placeholder="Search by name"></input>
-          <button type="submit"><i class="submit-search"></i></button>
+        <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="text" placeholder="Search by name"></input>
+          <input class="btn btn-secondary" type="submit" value="search"></input>
         </form>
       </header>
     </div>
