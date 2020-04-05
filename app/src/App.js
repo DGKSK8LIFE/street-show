ca31ce059430f59faceb0cce22bf617f68b3391d
Tarek Ali <tarekali15@outlook.com>
2020-04-05 11:10:22 -0500

    added custom purple button to fit the color theme

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
-import { TextField, makeStyles, Button } from '@material-ui/core';
+import { TextField, makeStyles, Button, withStyles } from '@material-ui/core';
+import { purple } from '@material-ui/core/colors';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -9,14 +10,20 @@ const useStyles = makeStyles((theme) => ({
       width: '25ch',
     },
   },
-  button: {
-    '& > *': {
-      margin: theme.spacing(1),
-      width: '10ch',
-    },
+  margin: {
+    margin: theme.spacing(),
   },
 }));
 
+const ColorButton = withStyles((theme) => ({
+  root: {
+    color: theme.palette.getContrastText(purple[500]),
+    backgroundColor: purple[500],
+    '&:hover': {
+      backgroundColor: purple[700],
+    },
+  },
+}))(Button);
 
 function App() {
   const classes = useStyles()
@@ -26,9 +33,9 @@ function App() {
       <header className="App-header">
         <form>
           <TextField className={classes.root} id="outlined-search" label="Search by name" varient="filled" type="search" size="large" />
-          <Button className={classes.button} variant="outlined" color="default" type="submit" size="large">
+          <ColorButton variant="contained" color="primary" className={classes.margin}>
             Search
-          </Button>
+          </ColorButton>
         </form>
       </header>
     </div>
