493ab4e9a5f282c98860f056d5469a53d702db5b
Tarek Ali <tarekali15@outlook.com>
2020-04-05 10:50:59 -0500

    added nice material input and button

--- app/src/App.js
+++ app/src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
-import { TextField, makeStyles } from '@material-ui/core';
+import { TextField, makeStyles, Button } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     '& .MuiTextField-root': {
       margin: theme.spacing(1),
       width: '25ch',
+    },
+  },
+  button: {
+    '& > *': {
+      margin: theme.spacing(1),
     },
   },
 }));
@@ -16,11 +21,13 @@ function App() {
   const classes = useStyles()
 
   return (
-    <div className={classes.root} style={{ display: `flex`, justifyContent: `center`, alignItems: `center` }}>
+    <div style={{ display: `flex`, justifyContent: `center`, alignItems: `center` }}>
       <header className="App-header">
         <form>
-          <TextField id="outlined-search" label="Search by name" varient="filled" type="search" />
-          <input class="btn btn-secondary btn-lg" type="submit" value="search"></input>
+          <TextField className={classes.root} id="outlined-search" label="Search by name" varient="filled" type="search" />
+          <Button className={classes.button} variant="outlined" color="default" type="submit" size="small">
+            Search
+          </Button>
         </form>
       </header>
     </div>
