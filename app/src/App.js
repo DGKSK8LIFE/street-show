3fc38f0cf8c6b7543c5db29e73b3a4afd3c2b794
Tarek Ali <tarekali15@outlook.com>
2020-04-05 10:22:07 -0500

    material UI import and such

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
 import './bootstrap.css';
+import '@material-ui/core';
 
 function App() {
   return (
-    <div className="App" style={{display: `flex`, justifyContent: `center`, alignItems: `center`}}>
+    <div className="App" style={{ display: `flex`, justifyContent: `center`, alignItems: `center` }}>
       <header className="App-header">
         <form class="form-inline my-2 my-lg-0">
           <input class="form-control form-control-lg" type="text" placeholder="Search by name"></input>
