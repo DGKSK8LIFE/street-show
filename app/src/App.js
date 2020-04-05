9e6c152e3cb005a726ce02f9194cd6ec5bcfcf80
Tarek Ali <tarekali15@outlook.com>
2020-04-05 11:26:48 -0500

    abstracted stylesheet js into another folder and exported its items

--- app/src/App.js
+++ app/src/App.js
@@ -1,29 +1,7 @@
 import React from 'react';
 import './App.css';
-import { TextField, makeStyles, Button, withStyles } from '@material-ui/core';
-import { purple } from '@material-ui/core/colors';
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    '& .MuiTextField-root': {
-      margin: theme.spacing(1),
-      width: '25ch',
-    },
-  },
-  margin: {
-    margin: theme.spacing(),
-  },
-}));
-
-const ColorButton = withStyles((theme) => ({
-  root: {
-    color: theme.palette.getContrastText(purple[500]),
-    backgroundColor: purple[500],
-    '&:hover': {
-      backgroundColor: purple[700],
-    },
-  },
-}))(Button);
+import { TextField,  } from '@material-ui/core';
+import { useStyles, ColorButton } from './Styles';
 
 function App() {
   const classes = useStyles()
