f993fd389845afec2c2ff1fcaab23e75ac4cfe04
Tarek Ali <tarekali15@outlook.com>
2020-04-06 09:43:03 -0500

    removed import alias

--- app/src/App.js
+++ app/src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import './App.css';
 import LandingPage from './LandingPage';
 import RestingReviewCard from './BuskerList';
-import { BrowserRouter as Router, Route } from 'react-dom';
+import { BrowserRouter, Route } from 'react-dom';
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <Route path="/" exact component={LandingPage} />
       <Route path="/buskerlist" exact component={RestingReviewCard} />
-    </Router>
+    </BrowserRouter>
   );
 }
 
