cb380cc8b88b8901e341954a320f7103cefcbeb5
Tarek Ali <tarekali15@outlook.com>
2020-04-07 10:05:35 -0500

    added basic router to main.go

---
+++ app/backend/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Content) {
+
+	})
+	r.POST("/buskerlist", func(c *gin.Content) {
+
+	})
+
+}
