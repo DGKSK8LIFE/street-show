9f175a1167c5759b0e94313b3d451c101803b17d
Tarek Ali <tarekali15@outlook.com>
2020-04-08 09:37:38 -0500

    idk

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import './App.css';
 import LandingPage from './LandingPage';
 import RecipeReviewCard from './BuskerList';
-import { BrowserRouter, Route } from 'react-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Route path="/" exact component={LandingPage} />
       <Route path="/buskerlist" exact component={RecipeReviewCard} />
-    </BrowserRouter>
+    </Router>
   );
 }
 
