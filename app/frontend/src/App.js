40c5f634648035f30f8c409529acbf3961f85c11
Tarek Ali <tarekali15@outlook.com>
2020-04-08 09:44:02 -0500

    removed routing

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import './App.css';
 import LandingPage from './LandingPage';
-import RecipeReviewCard from './BuskerList';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 
 function App() {
   return (
-    <Router>
-      <Route path="/" exact component={LandingPage} />
-      <Route path="/buskerlist" exact component={RecipeReviewCard} />
-    </Router>
+    <LandingPage />
   );
 }
 
