3c006b4f85a2160a091d7844e42d1bbccbe3572e
Tarek Ali <tarekali15@outlook.com>
2020-04-08 10:26:50 -0500

    added gorm imports and mysql mock opening

--- app/backend/main.go
+++ app/backend/main.go
@@ -1,11 +1,21 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dilacts/mysql"
+)
+
+var db 
+
+func init() {
+	db, err := gorm.Open("mysql", "[dbinfo]")
+}
 
 func main() {
 	r := gin.Default()
 	r.GET("/", func(c *gin.Content) {
-
+	
 	})
 	r.POST("/buskerlist", func(c *gin.Content) {
 
