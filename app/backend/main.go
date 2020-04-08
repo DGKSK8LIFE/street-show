14daa485833b9232b8bd9d8c77614587095d0050
Tarek Ali <tarekali15@outlook.com>
2020-04-08 11:13:49 -0500

    primer to using yaml file key-values as arg for db opening

--- app/backend/main.go
+++ app/backend/main.go
@@ -4,11 +4,28 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dilacts/mysql"
+	"fmt"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"log"
 )
+
+type DB_info struct {
+	User string  `yaml:"user"`
+	Password string `yaml:"password"`
+	Port uint32 	`yaml:"port"`
+	Db_name string 	`yaml:"db_name"`
+}
 
 var db 
 
 func init() {
+	infoStruct := DB_info{}
+	file, err := ioutil.ReadFile("db_info.yaml")
+	if err != nil {
+		log.Fatalf("database info file error: %s\n", err)	
+	}
+	err = yaml.Unmarshal(file, infoStruct)
 	db, err := gorm.Open("mysql", "[dbinfo]")
 }
 
