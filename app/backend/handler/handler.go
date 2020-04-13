9e6fef9aafd99cd3e27364767fd529406cc4a41e
Tarek Ali <tarekali15@outlook.com>
2020-04-13 11:50:55 -0500

    added same field to structs as columns to db

--- app/backend/handler/handler.go
+++ app/backend/handler/handler.go
@@ -3,8 +3,10 @@ package handler
 import "github.com/gin-gonic/gin"
 
 type User struct {
-	// empty for now as I need to think of what the user DB would entail
-	Index uint64
+	Username string
+	Name     string
+	Email    string
+	Id       uint64
 }
 
 func UserApi(c *gin.Context) {
