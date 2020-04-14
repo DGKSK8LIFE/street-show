e04f79851cb4cba4e3356506796b5d6a7accfd3d
Tarek Ali <tarekali15@outlook.com>
2020-04-14 08:03:16 -0500

    valid syntax now and busker table

--- app/backend/create_tables.sql
+++ app/backend/create_tables.sql
@@ -2,6 +2,14 @@ CREATE TABLE user (
 	username text,
 	name text,
 	email text,
-	id int NOT NULL AUTO_INCREMENT
+	id int NOT NULL AUTO_INCREMENT,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE busker (
+	username text,
+	name text,
+	email text,
+	id int NOT NULL AUTO_INCREMENT,
 	PRIMARY KEY (id)
 );
