6995469f8bfddd471f24421a4c274a630377c973
abhirup7 <abhirupsahoo@gmail.com>
2020-04-23 02:38:14 +0530

    temp searchbar

--- app/frontend/new/src/LandingPage.js
+++ app/frontend/new/src/LandingPage.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import EventsExample from './map'
+import { TextField, } from '@material-ui/core';
+import { useStyles, ColorButton } from './Styles';
+import './App.css';
+import SimpleBottomNavigation from './Footer';
+import Searchbox from './Searchbox';
 
 export default function LandingPage() {
+    const classes = useStyles()
+
     return (
-        <EventsExample />
+        <div class="wrapper">
+            <EventsExample />
+            <Searchbox />
+
+        </div>
     );
 }
