ad1e29462b869b7a0b891766bd6a173c2da98530
abhirup7 <abhirupsahoo@gmail.com>
2020-04-24 02:27:19 +0530

    material-ui searchbox

--- app/frontend/new/src/LandingPage.js
+++ app/frontend/new/src/LandingPage.js
@@ -1,19 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import EventsExample from './map'
-import { TextField, } from '@material-ui/core';
-import { useStyles, ColorButton } from './Styles';
+// import { TextField, } from '@material-ui/core';
+// import { useStyles, ColorButton } from './Styles';
 import './App.css';
-import SimpleBottomNavigation from './Footer';
+// import SimpleBottomNavigation from './Footer';
 import Searchbox from './Searchbox';
-
-export default function LandingPage() {
-    const classes = useStyles()
-
-    return (
-        <div class="wrapper">
-            <EventsExample />
-            <Searchbox />
-
-        </div>
-    );
+// import SearchBar from 'material-ui-search-bar'
+// import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+class LandingPage extends Component {
+    constructor() {
+        super()
+        this.state = {
+            searchfield: ' ',
+            buskers: [],
+        }
+    }
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event });
+    }
+    render() {
+        return (
+            <div class="wrapper">
+                <EventsExample />
+                <Searchbox searchChange={this.onSearchChange} />
+            </div>
+        );
+    }
 }
+export default LandingPage;
--- app/frontend/new/src/Searchbox.js
+++ app/frontend/new/src/Searchbox.js
@@ -1,24 +1,60 @@
-import React from 'react';
-import { TextField, } from '@material-ui/core';
-import { useStyles, ColorButton } from './Styles';
-import './App.css';
-import SimpleBottomNavigation from './Footer';
-export default function Searchbox() {
-    const classes = useStyles()
+import React from 'react'
+import SearchBar from 'material-ui-search-bar'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 
+const Searchbox = ({ searchfield, searchChange }) => {
     return (
 
-        <div class="searchbox" >
+        <MuiThemeProvider>
+            <SearchBar
+                // onClick={() => console.log("clicked")}
+                onChange={searchChange}
+                onRequestSearch={() => console.log('onRequestSearch')}
+                style={{
+                    maxWidth: 400,
+                    position: 'absolute',
+                    zIndex: 1000,
+                    left: 0,
+                    right: 0,
+                    marginLeft: 'auto',
+                    marginRight: 'auto',
+                    top: 10
+                }}
 
-            <header className="App-header">
-                <form>
-                    <TextField className={classes.root} id="outlined-search" placeholder="Search for Buskers near you" varient="filled" type="search" size="large" />
-                    <ColorButton variant="contained" color="primary" className={classes.margin} type="submit" id="button">
-                        Search
-                    </ColorButton>
-                </form>
-            </header>
-        </div>
+            />
 
+        </MuiThemeProvider>
     );
 }
+export default Searchbox;
+
+
+
+
+
+// import React from 'react';
+// import { TextField, } from '@material-ui/core';
+// import { useStyles, ColorButton } from './Styles';
+// import './App.css';
+// import SimpleBottomNavigation from './Footer';
+// export default function Searchbox() {
+//     const classes = useStyles()
+
+//     return (
+
+//         <div class="searchbox" >
+
+//             <header className="App-header">
+//                 <form>
+//                     <TextField className={classes.root} id="outlined-search" placeholder="Search for Buskers near you" varient="filled" type="search" size="large" />
+//                     <ColorButton variant="contained" color="primary" className={classes.margin} type="submit" id="button">
+//                         Search
+//                     </ColorButton>
+//                 </form>
+//             </header>
+//         </div>
+
+//     );
+// }
+
+
